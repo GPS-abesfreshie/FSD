4552eff0e4f9364fa48e89f8bc1b40461007261e
GPS-abesfreshie <gauravsinghpratap1234@gmail.com>
2026-08-19 14:37:41 +0530

    This is my learning

---
+++ DAY 1/index.js
@@ -0,0 +1,23 @@
+const app=require('./App.js')
+// var x=20;
+// var y=80;
+// var z=800;
+// console.log(x+y+z);
+
+// var x='20';
+// if(x===20){
+//   console.log('x is 20');
+// }
+// else{
+//   console.log('x is not 20');
+// }
+// for(var i=0;i<=10;){
+//   console.log(i++);
+// }
+// var x=20;
+// var x=[10,20,30,40,50];
+// for(let a=0;a<x.length;a++){
+//   console.log(x[a]);
+// }
+console.log(app.xz);
+console.log(app.yy);

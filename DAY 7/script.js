ab0c824660e204e7c3e41d6cbba42abdf8075eaf
GPS-abesfreshie <gauravsinghpratap1234@gmail.com>
2026-09-21 15:32:21 +0530

    This is my learning

---
+++ DAY 7/script.js
@@ -0,0 +1,49 @@
+class Employee {
+    constructor(id, name, basicSalary) {
+        this.basicSalary = basicSalary;
+    }
+
+    calculateSalary() {
+        return this.basicSalary;
+    }
+}
+
+// Derived class
+class Manager extends Employee {
+
+    constructor(id, name, basicSalary, incentive) {
+        super(id, name, basicSalary);
+        this.incentive = incentive;
+    }
+    // Method overriding
+    calculateSalary() {
+        return this.basicSalary + this.incentive;
+    }
+}
+
+// Creating Employee object
+let e1 = new Employee(101, "Utkarsh", 30000);
+let m1 = new Manager(102, "Rahul", 50000, 10000);
+
+// Display salaries
+console.log("Employee Salary:", e1.calculateSalary());
+console.log("Manager Salary:", m1.calculateSalary());
+
+//<--------------------------------------------------->//
+
+// Call Back Function
+function greetUser(name, callback) {
+    console.log("Hello " + name);
+    callback();
+}
+
+greetUser("Bob", function() {
+    console.log("Callback function executed!");
+});
+
+console.log("Start ")
+setTimeout(function( ){
+    console.log("Task Completed")
+},2000)
+
+console.log("End")

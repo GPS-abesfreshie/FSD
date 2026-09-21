0be05a1e4baeb7e0c43fee41eac1c168d20741a8
GPS-abesfreshie <gauravsinghpratap1234@gmail.com>
2026-09-21 15:05:54 +0530

    This is my learning

---
+++ DAY 4/index.js
@@ -0,0 +1,44 @@
+// const a={
+//   id:101,
+//   name:"Rana raj",
+//   show : function(){
+//     console.log("The name is " + this.name)
+//     console.log("The id is" + this.id)
+//   }
+// }
+// a.show()
+
+var A={
+  id:102,
+  name:"GAUR",
+  salary:10000,
+  incSal : function(){
+      this.salary=this.salary+10000
+  },
+  display : function(){
+    console.log(this.salary + " is the new salary of " + this.name)
+    
+  }
+}
+A.display()
+A.incSal()
+A.display()
+
+
+
+
+var college={
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
